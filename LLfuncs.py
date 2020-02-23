558faba42be9686ca85d0da89e1d017d33cee7a8
ProgrammingForFun <56187517+phanmemmaytinhtql@users.noreply.github.com>
2020-02-23 19:52:06 +0700

    Add files via upload

---
+++ LLfuncs.py
@@ -0,0 +1,21 @@
+def longest_al_sub(string):
+    n = len(string)
+    i = j = 0
+    max_len = 0
+
+    while i <= n - 1:
+        temp_len = 0
+        while True:
+            temp_len += 1
+            j += 1
+            if  j > n - 1 or string[j] < string[j - 1]:
+                break
+        if temp_len > max_len:
+            max_len = temp_len
+            list_of_sub = [string[i:j]]
+        elif temp_len == max_len:
+            list_of_sub.append(string[i:j])
+        i = j
+        
+    return list_of_sub
+
